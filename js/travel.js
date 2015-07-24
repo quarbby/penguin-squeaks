0968e0b1491ea961956a0826c609a0c78d37562e
Lynnette Ng <lynnette-ng@outlook.com>
2015-07-24 04:32:00 +0000

    Added Europe travels

--- js/travel.js
+++ js/travel.js
@@ -1,12 +1,12 @@
 window.onload = function() {
-    var map = L.map('map').setView([1.300, 103.8], 2);
+    var map = L.map('map').setView([23, 10], 2);
     
     L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
         subdomains: ['otile1','otile2','otile3','otile4'],
     	minZoom: 0,
     	maxZoom: 20,
-    	ext: 'png'
+    	ext: 'png',
     }).addTo(map);
     
     var worldmaptitle = L.control({position: 'topright'});
